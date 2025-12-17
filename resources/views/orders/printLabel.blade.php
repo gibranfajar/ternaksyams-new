7be59fe5767ade871bc320036e667b9aba147338
gibranfajar <gibransatritamaa@gmail.com>
2025-12-17 17:19:43 +0700

    update jquery printlabel

--- resources/views/orders/printLabel.blade.php
+++ resources/views/orders/printLabel.blade.php
@@ -54,6 +54,10 @@
 @endsection
 
 @push('scripts')
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
+
     <script>
         $(function() {
             $('#checkAll').on('change', function() {
