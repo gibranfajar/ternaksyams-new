a6c1f61013dd34856ac76f13ed7e9b8405bfb04e
gibranfajar <gibransatritamaa@gmail.com>
2025-12-19 21:31:46 +0700

    update trix editor

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -48,6 +48,10 @@
     <!-- ApexCharts -->
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 
+    {{-- Trix Editor --}}
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.8/dist/trix.css">
+    <script type="text/javascript" src="https://unpkg.com/trix@2.0.8/dist/trix.umd.min.js"></script>
+
     @stack('styles')
 
     <!-- FIX SEARCH KEHIDE TEMPLATE -->
